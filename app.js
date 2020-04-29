a2f635b7a67afc54ca484a910e4f230fffc1486b
Maxim Demidov <maaksaa@gmail.com>
2020-04-29 22:37:51 +0300

    Добавил отображение русских новостей про технологии

--- app.js
+++ app.js
@@ -56,7 +56,69 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
+const newsService = (function () {
+  const apiKey = "653c3600ddc34ff9a81a7f0ded4421fd";
+  const apiUrl = "https://newsapi.org/v2";
+
+  return {
+    topHeadlines(country = "ru", cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+        cb
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+
 //  init selects
 document.addEventListener("DOMContentLoaded", function () {
   M.AutoInit();
+  loadNews();
 });
+
+// load news function
+function loadNews() {
+  newsService.topHeadlines("ru", onGetResponse);
+}
+
+// function on get response from server
+function onGetResponse(err, res) {
+  renderNews(res.articles);
+}
+
+// function render news
+function renderNews(news) {
+  const newsContainer = document.querySelector(".news-container .row");
+  let fragment = "";
+
+  news.forEach((newsItem) => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+
+  newsContainer.insertAdjacentHTML("afterbegin", fragment);
+}
+
+// News item template
+function newsTemplate({ urlToImage, title, url, description }) {
+  return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage}">
+          <span class="card-title">${title || ""}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ""}</p>
+        </div>
+        <div class="card-action">
+          <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+
+  `;
+}
